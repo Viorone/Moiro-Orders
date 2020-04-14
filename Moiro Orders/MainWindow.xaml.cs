9941fd39c39500319992675c9c2ec0b932343976
Viorone <viorone1997@gmail.com>
2020-04-14 12:16:28 +0300

    v 0.27 open beta

--- Moiro Orders/MainWindow.xaml.cs
+++ Moiro Orders/MainWindow.xaml.cs
@@ -200,6 +200,7 @@ namespace Moiro_Orders
 
         internal static User Im = new User();
         internal static int sortCount = -1;
+        internal static string version = "0.26 beta";
         internal static CancellationTokenSource ordersCts = new CancellationTokenSource();       
         static PublicResources()
         {
