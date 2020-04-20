eff49c16e8ab55250a9d58ce96437d62cb4d9e7b
Viorone <viorone1997@gmail.com>
2020-04-20 16:59:48 +0300

    v 0.28.1 Error Messages for Event

--- Moiro Orders/XamlView/EventView.xaml.cs
+++ Moiro Orders/XamlView/EventView.xaml.cs
@@ -21,26 +21,10 @@ namespace Moiro_Orders.XamlView
 
         private void AddAllEvents_Click(object sender, RoutedEventArgs e)
         {
+            FormAddEvent.Visibility = Visibility.Visible;
             // при добавлении должен быть указан кабинет у пользователя!
             FormAddEvent.Visibility = Visibility.Visible;
-            async Task SetEventsOfDate()
-                {
-                    IUser user = new CurrentUser();
-                    var status = await user.CreateEvent(new Event
-                    {
-                        Date = DateTime.Now,
-                        Description = "Бысл сломан компьютер последством внешнего вмешательства сверхестественных сил",
-                        UserId = PublicResources.Im.Id,
-                        DateStart = DateTime.Now,
-                        DateEnd = DateTime.Now,
-                        NameEvent = "Карамба!!!",
-                        Place = "Уютный домик",
-                        StatusId = 1
-                    });
-                    MessageBox.Show(status.ToString());
-                }
-               // SetEventsOfDate().GetAwaiter();
-               // отключил верхний таск что бы не спамили
+         
            
         }
 
@@ -74,21 +58,77 @@ namespace Moiro_Orders.XamlView
             }
         }
 
-        private void BackToEvent_Click(object sender, RoutedEventArgs e)
-        {
-            FormAddEvent.Visibility = Visibility.Hidden;
-        }
-
         private void SelectedDatesShow_Click(object sender, RoutedEventArgs e)
         {
-            var collections = CalendarWhithDate.SelectedDates;
-            foreach (var tmpDate in collections)
+            if (CheckFields())
             {
-               // stackTrace.Text += tmpDate.Date+" ";
+                var collections = CalendarWhithDate.SelectedDates;
+                foreach (var tmpDate in collections)
+                {
+                    async Task SetEventsOfDate()
+                    {
+                        DateTime startDate, endDate;
+                        startDate = tmpDate;
+                        startDate = startDate.AddHours(StartTime.SelectedTime.Value.Hour);
+                        startDate = startDate.AddMinutes(StartTime.SelectedTime.Value.Minute);
+                        endDate = tmpDate;
+                        endDate = startDate.AddHours(StartTime.SelectedTime.Value.Hour);
+                        endDate = startDate.AddMinutes(StartTime.SelectedTime.Value.Minute);
+                        IUser user = new CurrentUser();
+                        var status = await user.CreateEvent(new Event
+                        {
+                            Date = DateTime.Now,
+                            Description = DescriptionEvent.Text,
+                            UserId = PublicResources.Im.Id,
+                            DateStart = startDate,
+                            DateEnd = endDate,
+                            NameEvent = NameEvent.Text,
+                            Place = PlaceEvent.Text,
+                            StatusId = 1
+                        });
+                        MessageBox.Show(status.ToString());
+                    }
+                    SetEventsOfDate().GetAwaiter();
+                }
             }
         }
+        //проверка на заполненость полей для мероприятия
+        private bool CheckFields()
+        {
+            if (NameEvent.Text == "" || NameEvent.Text == null)
+            {
+                ShowErrorMessage("Ошибка в названии мероприятия", "Введите название предстоящего мероприятия");
+                return false;
+            }
+            if (PlaceEvent.Text == "" || PlaceEvent.Text == null)
+            {
+                ShowErrorMessage("Ошибка в месте проведения", "Введите место проведения предстоящего мероприятия");
+                return false;
+            }
+            if (CalendarWhithDate.SelectedDates.Count == 0)
+            {
+                ShowErrorMessage("Ошибка в дате мероприятий", "Выберите дату или даты проведения данного мероприятия");
+                return false;
+            }
+            if (!StartTime.SelectedTime.HasValue && !EndTime.SelectedTime.HasValue)
+            {
+                ShowErrorMessage("Ошибка во времени проведения", "Выберите время начала и окончания проведения данного мероприятия");
+                return false;
+            }
+            return true;
+        }
+        // Сообщение об ошибке
+        private void ShowErrorMessage(string header, string body)
+        {
+            ErrorGrid.Visibility = Visibility.Visible;
+            ErrorEventHeader.Text = header;
+            ErrorEventBody.Text = body;
+        }
+        private void ErrorOk_Click(object sender, RoutedEventArgs e)
+        {
+            ErrorGrid.Visibility = Visibility.Hidden;
+        }
 
-        // 
         private void CalendarWhithDate_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
         {
             if (CalendarWhithDate.SelectedDates.Count >= 31)
@@ -96,5 +136,7 @@ namespace Moiro_Orders.XamlView
                 MessageBox.Show("Выбрано слишком много дат!");
             }
         }
+
+       
     }
 }
