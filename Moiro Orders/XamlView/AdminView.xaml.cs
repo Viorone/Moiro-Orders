9750ebed12969f4eee80392dc3425e0b6ef6cc23
Viorone <viorone1997@gmail.com>
2020-05-05 17:14:43 +0300

    v 0.33

--- Moiro Orders/XamlView/AdminView.xaml.cs
+++ Moiro Orders/XamlView/AdminView.xaml.cs
@@ -4,7 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-
+using Moiro_Orders.Models;
+using System;
 
 namespace Moiro_Orders.XamlView
 {
@@ -16,11 +17,13 @@ namespace Moiro_Orders.XamlView
         public AdminView()
         {
             InitializeComponent();
-            IAdmin admin = new CurrentUser();
-            admin.GetCountOrdersByStatus(1).GetAwaiter();
+            for (int i = 1; i < 6; ++i)
+            {
+                GetCountOrders(i).GetAwaiter();
+            }
         }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
+        
+        private void Button_Click(object sender, RoutedEventArgs e) //Гигантская кнопка
         {
             async Task UpdateUsersDb()
             {
@@ -40,30 +43,141 @@ namespace Moiro_Orders.XamlView
                     var response = await admin.UpdateUsersDb(var1);
                     responses.Add(response.ToString());
                     logList.Items.Add(response.ToString());
-                }               
+                }
             }
             UpdateUsersDb().GetAwaiter();
         }
-        bool refresh = false;
+
+        #region Для вкладки "Статистика"
+
+        #region Обработчики событий на карточки
+        bool refresh = false; //нужно для измежания нажатия на карточку в которую входит обновление цифр
         private void CardOrdersNew_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
             if (!refresh)
             {
-                //main code
-                MessageBox.Show("Card");
+                GetOrdersByStatus(1, dateStart.SelectedDate.Value, dateEnd.SelectedDate.Value).GetAwaiter();
             }
             else
             {
                 refresh = false;
             }
         }
+        private void CardOrdersCancel_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (!refresh)
+            {
+                GetOrdersByStatus(2, dateStart.SelectedDate.Value, dateEnd.SelectedDate.Value).GetAwaiter();
+            }
+            else
+            {
+                refresh = false;
+            }
+        }
+        private void CardOrdersComplete_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (!refresh)
+            {
+                //main code
+                MessageBox.Show("CardComplete");
+            }
+            else
+            {
+                refresh = false;
+            }
+        }
+        private void CardOrdersNeedRepair_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (!refresh)
+            {
+                //main code
+                MessageBox.Show("CardNeedRepair");
+            }
+            else
+            {
+                refresh = false;
+            }
+        }
+        private void CardOrdersInProgress_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (!refresh)
+            {
+                //main code
+                MessageBox.Show("CardШтЗкщпкуыы");
+            }
+            else
+            {
+                refresh = false;
+            }
+        }
+        #endregion
 
+        #region Обработчики событий на обновление карточки
         private void RefreshOrdersNew_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
             refresh = true;
-            //code to refresh
-            MessageBox.Show("refresh");
-
+            GetCountOrders(1).GetAwaiter();
         }
+        private void RefreshOrdersinProgress_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            refresh = true;
+            GetCountOrders(2).GetAwaiter();
+        }
+        private void RefreshOrdersComplete_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            refresh = true;
+            GetCountOrders(3).GetAwaiter();
+        }
+        private void RefreshOrdersNeedRepair_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            refresh = true;
+            GetCountOrders(4).GetAwaiter();
+        }
+        private void RefreshOrdersCancel_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            refresh = true;
+            GetCountOrders(5).GetAwaiter();
+        }
+        #endregion
+
+        #endregion
+
+        #region ASYNC metods
+        async Task<int> GetCountOrders(int statusId)
+        {
+            IAdmin admin = new CurrentUser();
+            var count = await admin.GetCountOrdersByStatus(statusId);
+            switch (statusId)
+            {
+                case 1:
+                    CountOrdersNew.Text = count.ToString();
+                    break;
+                case 2:
+                    CountOrdersInProgress.Text = count.ToString();
+                    break;
+                case 3:
+                    CountOrdersComplete.Text = count.ToString();
+                    break;
+                case 4:
+                    CountOrdersNeedRepair.Text = count.ToString();
+                    break;
+                case 5:
+                    CountOrdersCancel.Text = count.ToString();
+                    break;
+               
+            }
+            return count;
+        }
+
+        async Task GetOrdersByStatus(int statusId, DateTime dateStart, DateTime dateEnd)
+        {
+            IAdmin admin = new CurrentUser();
+            var orders = await admin.GetOrdersByStatus(statusId, dateStart, dateEnd);
+            ListGettingOrders.ItemsSource = orders;
+        }
+
+        #endregion
+
+       
     }
 }
